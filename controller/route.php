3146d58232cc842d0e81ad9fe9185dca100bc64b
Adrrien04 <adrrien.chandrakumar@gmail.com>
2024-01-11 18:46:21 +0100

    ajout pages front

---
+++ controller/route.php
@@ -0,0 +1,17 @@
+<?php
+function getPageContent($page) {
+  if ($page == 'accueil') {
+    include_once 'view/accueil.php';
+  } elseif ($page == 'lancer-un-appel') {
+    include_once 'view/lancer-un-appel.php';
+  } elseif ($page == 'emojiShop') {
+    include_once 'view/emojiShop.php';
+  } elseif ($page == 'signup') {
+    include_once 'view/signup.php';
+  }  elseif ($page == 'login') {
+    include_once 'view/login.php';
+  } else {
+    include_once 'view/accueil.php';
+  }
+}
+?>
